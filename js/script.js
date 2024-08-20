615a79034db00ff2e066790022283e56b0ff35ca
hanion77 <speciic77@naver.com>
2024-08-20 20:08:27 +0900

    script 수정

--- js/script.js
+++ js/script.js
@@ -18,3 +18,54 @@ for (let pm of panelMenu) {
     });
   });
 }
+
+// 슬라이드 초기화
+slides.forEach((slide, index) => {
+  slide.style.left = '100%'; // 모든 슬라이드를 화면 밖에 위치시킴
+});
+slides[0].style.left = '0'; // 첫 번째 슬라이드를 화면에 보이게 설정
+
+function showSlide(num) {
+  if (currentIdx === num) return;
+  
+  let currentSlide = slides[currentIdx];
+  let nextSlide = slides[num];
+  
+  nextSlide.style.left = '100%';
+
+  nextSlide.animate(
+    [
+      { left: '100%' },
+      { left: '0%' },
+    ],
+    { duration: 500, fill: 'forwards' }
+  );
+
+  currentSlide.animate(
+    [
+      { left: '0%' },
+      { left: '-100%' },
+    ],
+    { duration: 500, fill: 'forwards' }
+  );
+
+  currentIdx = num;
+}
+showSlide(0);
+
+function autoSlide() {
+  if (timer) clearInterval(timer);
+
+  timer = setInterval(() => {
+    let nextIdx = (currentIdx + 1) % slideCount;
+    showSlide(nextIdx);
+  }, 3000);
+}
+autoSlide();
+
+slideWrapper.addEventListener('mouseenter', () => {
+  clearInterval(timer);
+});
+slideWrapper.addEventListener('mouseleave', () => {
+  autoSlide();
+});
