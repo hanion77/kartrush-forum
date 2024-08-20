e2453b821663f5c2e410cc15417afb43ad6db8cb
hanion77 <speciic77@naver.com>
2024-08-20 18:01:20 +0900

    script 수정

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 const panelMenu = document.querySelectorAll(".panel_menu");
+const slideWrapper = document.querySelector('.slidewrapper');
+const slideContainer = slideWrapper.querySelector('.slidecontainer');
+const slides = slideWrapper.querySelectorAll('.slide');
+const slideCount = slides.length;
+let currentIdx = 0;
+let timer;
+
 
 for (let pm of panelMenu) {
-  pm.addEventListener("click", (e) => {
+  pm.addEventListener("click", () => {
     pm.classList.toggle("active");
 
     panelMenu.forEach((item) => {
@@ -11,3 +18,20 @@ for (let pm of panelMenu) {
     });
   });
 }
+
+
+function autoSlide(){
+  timer = setInterval(()=>{
+    let nextIdx = (currentIdx + 1)%slideCount;
+
+    showSlide(nextIdx);
+  }, 3000);
+}
+autoSlide();
+
+slideWrapper.addEventListener('mouseenter',()=>{
+  clearInterval(timer);
+})
+slideWrapper.addEventListener('mouseleave',()=>{
+  autoSlide();
+})
