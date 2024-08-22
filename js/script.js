4e539ccdcc83a165cad0257cb650381029f97cc8
hanion77 <speciic77@naver.com>
2024-08-22 11:31:03 +0900

    script 수정

--- js/script.js
+++ js/script.js
@@ -45,3 +45,42 @@ slideWrapper.addEventListener("mouseenter", () => {
 slideWrapper.addEventListener("mouseleave", () => {
   autoSlide();
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const scrollContainer = document.querySelector('.horizontal_scroll');
+
+  scrollContainer.addEventListener('wheel', (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+  });
+
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  scrollContainer.addEventListener('mousedown', (e) => {
+    isDown = true;
+    scrollContainer.classList.add('active');
+    startX = e.pageX - scrollContainer.offsetLeft;
+    scrollLeft = scrollContainer.scrollLeft;
+  });
+
+  scrollContainer.addEventListener('mouseleave', () => {
+    isDown = false;
+    scrollContainer.classList.remove('active');
+  });
+
+  scrollContainer.addEventListener('mouseup', () => {
+    isDown = false;
+    scrollContainer.classList.remove('active');
+  });
+
+  scrollContainer.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - scrollContainer.offsetLeft;
+    const walk = (x - startX) * 2; // 스크롤 속도 조정
+    scrollContainer.scrollLeft = scrollLeft - walk;
+  });
+});
